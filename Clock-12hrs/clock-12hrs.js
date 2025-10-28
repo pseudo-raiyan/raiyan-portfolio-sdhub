07b579e5239dfb8c02a3d23a4742438671912b8a
pseudo-raiyan <raiyanak3@gmail.com>
2025-10-28 11:29:06 +0530

    clock

--- Clock-12hrs/clock-12hrs.js
+++ Clock-12hrs/clock-12hrs.js
@@ -1,6 +1,6 @@
 setInterval(() => {
 const now= new Date();
-const hour= now.getHours()%12
+const hour= now.getHours()%12 || 12
 const minute= now.getMinutes()
 const sec= now.getSeconds()
 document.querySelector('#time-here').textContent= (`${hour} : ${minute} : ${sec}`)
